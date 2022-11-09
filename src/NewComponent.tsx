6155cd7f7a283c74be837e94c2d871252905072e
lya-melnik <ilay.melnik.kr@gmail.com>
2022-11-09 14:39:42 -0800

    output an array to a table using the map method

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+
 type NewComponentType = {
     students: Array<StudentsType>
 }
+
 type StudentsType = {
     id: number, name: string, age: number
 }
@@ -10,18 +12,29 @@ export const NewComponent = (props: NewComponentType) => {
     return (
         <div>
             <ul>
-                {props.students.map((objectFromStudentArray, index)=>{
-                   return (<li key={objectFromStudentArray.id}>
-                       <span>{objectFromStudentArray.name}</span>
-                       <span> age: {objectFromStudentArray.age}</span>
+                {props.students.map((objectFromStudentArray, index) => {
+                    return (<li key={objectFromStudentArray.id}>
+                        <span>{objectFromStudentArray.name}</span>
+                        <span> age: {objectFromStudentArray.age}</span>
                     </li>)
                 })}
-
-
                 <li>wqwqw</li>
                 <li>wwqwq</li>
                 <li>qwqw</li>
             </ul>
+
+
+            <table>
+                    {props.students.map((st, index) => {
+                        return (
+                            <tr>
+                                <td>{index+1}</td>
+                                <td>{st.name}</td>
+                                <td>{st.age}</td>
+                            </tr>)
+
+                    })}
+            </table>
         </div>
     );
 };
